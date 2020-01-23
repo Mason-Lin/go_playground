f6dffd6cc2b788d5ca40d3a954bda50a04a85c89
Mason Lin <pizza0117@gmail.com>
2020-01-23 21:50:17 +0800

    done helloworld try use global, local vars will do 9x9 table

---
+++ var.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var (
+	glo_a int
+	glo_b bool
+)
+
+func main() {
+	var loc_a int = 1
+	co_a, co_b := 2, 3
+	fmt.Println("Hello", glo_a, glo_b, loc_a, co_a, co_b)
+}
